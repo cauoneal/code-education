af65b1e8d086eb8ce89145c98729887eea222641
cau <cau@cau>
2016-11-22 13:14:25 -0300

    SEGUNDA FASE DO PROJETO

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -6,23 +6,23 @@
 
 var app = angular.module('app', ['ngRoute','angular-oauth2', 'app.controllers']);
 
-angular.module('app.controllers', ['angular-oauth2']);
+angular.module('app.controllers', ['ngMessages','angular-oauth2']);
 
 app.config(['$routeProvider','OAuthProvider', function($routeProvider,OAuthProvider) {
     $routeProvider
             .when('/login', {
                 templateUrl: 'build/views/login.html',
-                contorller: 'LoginController'
+                controller: 'LoginController'
             })
             .when('/home', {
                 templateUrl: 'build/views/home.html',
-                contorller: 'HomeController'
+                controller: 'HomeController'
             });        
     OAuthProvider.configure({
       baseUrl: 'http://localhost:8000',
-      clientId: 'app',
+      clientId: 'appid1',
       clientSecret: 'secret', // optional
-      grant_path: 'oauth/access_token'
+      grantPath: 'oauth/access_token'
     });
   }]);
   
@@ -38,7 +38,7 @@ app.run(['$rootScope', '$window', 'OAuth', function($rootScope, $window, OAuth) 
       if ('invalid_token' === rejection.data.error) {
         return OAuth.getRefreshToken();
       }
-      console.log(rejection.data.error);
+      console.log($rootScope);
       // Redirect to `/login` with the `error_reason`.
       return $window.location.href = '/login?error_reason=' + rejection.data.error;
     });
